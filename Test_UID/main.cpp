ea6db6f311dde8a6a45601de993db7195a2e258a
forouher <forouher@iti.uni-luebeck.de>
2010-05-01 13:03:23 +0000

    adapt Test_UID to new Module_UID code
    git-svn-id: svn+ssh://elmi.iti.uni-luebeck.de/export/svn/Hanse/trunk@63 8f13a6d9-6203-4c43-9b9f-a3039379152f

--- Test_UID/main.cpp
+++ Test_UID/main.cpp
@@ -9,17 +9,17 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    Module_UID *uid = new Module_UID("uid1","UIDC0001");
+    Module_UID *uid = new Module_UID("uid1");
 
-    if ( !(uid->UID_Available()) ) {
+    if ( !(uid->getUID()->UID_Available()) ) {
         cerr << "No UID found" << endl;
     }
     else {
-        cout << "Identification: " <<uid->UID_Identify().toStdString() << endl;
-        cout << "Revision: " << uid->UID_Revision().toStdString() << endl;
+        cout << "Identification: " <<uid->getUID()->UID_Identify().toStdString() << endl;
+        cout << "Revision: " << uid->getUID()->UID_Revision().toStdString() << endl;
 
         unsigned char result[2];
-        if (uid->UID_ADC(0b00001010,result)) {
+        if (uid->getUID()->UID_ADC(0b00001010,result)) {
             cout << result[0] << endl;
             cout << result[1] << endl;
         }
