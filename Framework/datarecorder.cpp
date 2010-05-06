0643ee9512e32887e0b3bfa0df91e88f8d19a974
forouher <forouher@iti.uni-luebeck.de>
2010-05-06 14:47:24 +0000

    maybe also add the actual code...
    git-svn-id: svn+ssh://elmi.iti.uni-luebeck.de/export/svn/Hanse/trunk@123 8f13a6d9-6203-4c43-9b9f-a3039379152f

---
+++ Framework/datarecorder.cpp
@@ -0,0 +1,69 @@
+#include "datarecorder.h"
+
+DataRecorder::DataRecorder(RobotModule& module)
+    : module(module)
+{
+    logger = Log4Qt::Logger::logger("DataRecorder");
+
+    connect(&module, SIGNAL(dataChanged(RobotModule*)), this, SLOT(newDataReceived(RobotModule*)));
+
+    path = "logs/"+QDateTime::currentDateTime().toString("yyyy-MM-dd_HH-mm");
+    QDir(".").mkpath(path);
+    file = new QFile(path+"/"+module.getId()+".csv");
+}
+
+void DataRecorder::open()
+{
+    QStringList dataKeysNew = module.getData().keys();
+    dataKeysNew.sort();
+
+    QStringList settingsKeysNew = module.getSettings().allKeys();
+    settingsKeysNew.sort();
+
+    // TODO: compare lists
+    bool listsChanged = false;
+
+    if (file->isOpen() && listsChanged) {
+        file->close();
+        fileCount++;
+        file = new QFile(path+"/"+module.getId()+"_"+QString::number(fileCount)+".csv");
+        stream = NULL;
+    }
+
+    if (!file->isOpen()) {
+        if (file->open(QFile::WriteOnly | QFile::Truncate)) {
+            stream = new QTextStream(file);
+        } else {
+            logger->error("Could not open file "+file->fileName());
+        }
+
+        dataKeys = module.getData().keys();
+        dataKeys.sort();
+
+        settingsKeys = module.getSettings().allKeys();
+        settingsKeys.sort();
+    }
+
+}
+
+void DataRecorder::newDataReceived(RobotModule *module)
+{
+    open();
+
+    if (!stream) {
+        return;
+    }
+
+    foreach (QString key, settingsKeys) {
+        *stream << module->getSettings().value(key).toString() << ",";
+    }
+
+    foreach (QString key, dataKeys) {
+        *stream << module->getData().value(key).toString() << ",";
+    }
+}
+
+void DataRecorder::close()
+{
+    file->close();
+}
