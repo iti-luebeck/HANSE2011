82d1e1228492ae297079b5f754821e932a7f59c5
forouher <forouher@iti.uni-luebeck.de>
2010-05-14 13:59:30 +0000

    module_uid rewrite
    git-svn-id: svn+ssh://elmi.iti.uni-luebeck.de/export/svn/Hanse/trunk@166 8f13a6d9-6203-4c43-9b9f-a3039379152f

--- Hanse/Module_UID/module_uid.cpp
+++ Hanse/Module_UID/module_uid.cpp
@@ -3,28 +3,40 @@
 #include "module_uid.h"
 #include "form_uid.h"
 #include "QtUID.h"
+#include <qextserialport.h>
+#include <qextserialenumerator.h>
 
 Module_UID::Module_UID(QString moduleId)
     :RobotModule(moduleId)
 {
+
+    portSettings = new PortSettings();
+    portSettings->BaudRate = BAUD115200;
+    portSettings->DataBits = DATA_8;
+    portSettings->Parity = PAR_NONE;
+    portSettings->StopBits = STOP_1;
+    portSettings->FlowControl = FLOW_OFF;
+    portSettings->Timeout_Millisec = 500;
+
     setDefaultValue("uidId", "UIDC0001");
 
-    uid = new UID(settings.value("uidId").toString());
-    if (!uid->UID_Available())
-        setHealthToSick("UID not available.");
+    uid = NULL;
+    reset();
 }
 
 Module_UID::~Module_UID()
 {
-    delete uid;
+    // TODO
 }
 
 void Module_UID::reset()
 {
-    RobotModule::reset();
-    uid->ClosePort();
-    delete uid;
-    uid = new UID(settings.value("uidId").toString());
+    if (uid != NULL) {
+        uid->close();
+        delete uid;
+        uid = NULL;
+    }
+    uid = findUIDPort();
 }
 
 QList<RobotModule*> Module_UID::getDependencies()
@@ -40,10 +52,222 @@ QWidget* Module_UID::createView(QWidget* parent)
 void Module_UID::terminate()
 {
     RobotModule::terminate();
-    //
 }
 
-UID* Module_UID::getUID()
+QextSerialPort* Module_UID::findUIDPort()
 {
-    return uid;
+    QString Id = settings.value("uidId").toString();
+
+    QList<QextPortInfo> ports = QextSerialEnumerator::getPorts();
+
+    logger->info("Trying out all available serial ports");
+
+    QextSerialPort *p = NULL;
+
+    foreach (QextPortInfo port, ports) {
+            logger->debug("port name: "+port.portName);
+            logger->debug("friendly name: "+ port.friendName);
+            logger->debug("physical name: "+ port.physName);
+            logger->debug("enumerator name: "+ port.enumName);
+            logger->debug("===================================");
+
+            p = tryOpenPort(Id, &port);
+    }
+
+    if (!p) {
+        setHealthToSick("Didn't find any UID.");
+    } else {
+        setHealthToOk();
+    }
+
+    return p;
+}
+
+QextSerialPort* Module_UID::tryOpenPort(QString Id, QextPortInfo *port)
+{
+//       port = new QextSerialPort(  "\\\\.\\"+ports.at(i).portName, *portSettings);
+    QextSerialPort* sport = new QextSerialPort( port->physName, *portSettings);
+
+       const char sequence[] = {UID::UID_IDENTIFY};
+       char id[9];
+
+       //if ( !(port->open(QextSerialPort::ReadWrite) ) ) {
+       if ( !(sport->open(QIODevice::ReadWrite | QIODevice::Unbuffered) ) ) {
+           logger->debug("Could not connect: "+sport->errorString());
+           sport->close();
+           delete sport;
+           return NULL;
+       }
+
+       logger->debug("connected successfully");
+
+       sport->flush(); // ???
+
+       if ( (sport->write(sequence, 1)) == -1) {
+           logger->debug("Writing Opcode failed");
+           sport->close();
+           delete sport;
+           return NULL;
+       }
+
+       logger->debug("Could write to port");
+       int bytesRead = sport->read(id, sizeof(id));
+       if ( bytesRead < sizeof(id)) {
+           logger->debug("No Id received, read only "+QString::number(bytesRead)+ " bytes.");
+           sport->close();
+           delete sport;
+           return NULL;
+       }
+
+       if (QString::fromAscii(id,sizeof(id)-1) != Id) {
+           logger->debug("Received id: " + QString::fromUtf8(id,8)+"; expected: "+Id);
+           logger->debug("Wrong Id\r\nReceived: " + QString::fromUtf8(id,8));
+           sport->close();
+           delete sport;
+           return NULL;
+       }
+
+       logger->debug("Found UID with id " + QString::fromAscii(id,8) + " on " + port->portName);
+       sport->flush();
+       return sport;
+
+}
+
+bool Module_UID::I2C_WriteRegister(unsigned char address, unsigned char reg, unsigned char* data, short byteCount) {
+    if (!uid) {
+        logger->error("Can't do. UID not open.");
+        return false;
+    }
+
+    //unsigned char sequence[4+byteCount];// = {UID::I2C_WRITEREGISTER, address, reg, (unsigned char)byteCount};
+    unsigned char sequence[] = {UID::I2C_WRITEREGISTER, address, reg, (unsigned char)byteCount};
+    /*
+    int pos = 0;
+    sequence[pos++] = UID::I2C_WRITEREGISTER;
+    sequence[pos++] = address;
+    sequence[pos++] = reg;
+    sequence[pos++] = (unsigned char)byteCount;
+
+    for (int i=0; i<byteCount; i++)
+        sequence[pos++]=data[i];
+        */
+    if (!SendCommand(sequence, sizeof(sequence), 0)) return false;
+    if (!SendCommand(data, byteCount, 0)) return false;
+    return true;
+}
+
+bool Module_UID::I2C_ReadRegisters(unsigned char address, unsigned char reg, short byteCount, unsigned char* result) {
+    if (!uid) {
+        logger->error("Can't do. UID not open.");
+        return false;
+    }
+
+    unsigned char sequence[] = {UID::I2C_READREGISTER, address, reg, byteCount};
+    if (!SendCommand(sequence, sizeof(sequence), 0)) return false;
+    if ( (uid->read((char*)result, byteCount)) != byteCount) return false;
+    return true;
+}
+
+
+bool Module_UID::SendCommand(unsigned char* sequence, unsigned char length, int msec) {
+    if (!uid) {
+        logger->error("Can't do. UID not open.");
+        return false;
+    }
+
+    if ( (uid->write( (const char*)sequence, length )) == -1 ) return false;
+    uid->flush();
+    return true;
+}
+
+bool Module_UID::I2C_Write(unsigned char address, unsigned char* data, short byteCount) {
+    if (!uid) {
+        logger->error("Can't do. UID not open.");
+        return false;
+    }
+
+    unsigned char sequence[] = {UID::I2C_WRITE, address, byteCount};
+    if (!SendCommand(sequence, sizeof(sequence), 0)) return false;
+    if (!SendCommand(data, byteCount, 0)) return false;
+    return true;
+}
+
+bool Module_UID::I2C_Read(unsigned char address, short byteCount, unsigned char* result) {
+    if (!uid) {
+        logger->error("Can't do. UID not open.");
+        return false;
+    }
+
+    unsigned char sequence[] = {UID::I2C_READ, address, byteCount};
+    if (!SendCommand(sequence, sizeof(sequence) , 0)) return false;
+    if ( (uid->read((char*)result, byteCount)) != byteCount) return false;
+    return true;
+}
+
+QString Module_UID::UID_Identify() {
+    if (!uid) {
+        logger->error("Can't do. UID not open.");
+        return "";
+    }
+
+    QString identify;
+    unsigned char sequence[] = {UID::UID_IDENTIFY};
+    char result[9];
+    if ( !(SendCommand(sequence, sizeof(sequence), 0)) ) return NULL;
+    qint64 ret = uid->read(result,sizeof(result));
+    if( ret == -1 ) return NULL;
+    return QString::fromAscii(result, ret);
+}
+
+QString Module_UID::UID_Revision() {
+    if (!uid) {
+        logger->error("Can't do. UID not open.");
+        return "";
+    }
+
+    QString identify;
+    unsigned char sequence[] = {UID::UID_REVISION};
+    char result[8];
+    if ( !(SendCommand(sequence, sizeof(sequence), 0)) ) return NULL;
+    //if (port->read(result,sizeof(result)) == -1) return NULL;
+    qint64 ret = uid->read(result,sizeof(result));
+    if (ret==-1) return NULL;
+    //return QString(result);
+    return QString::fromAscii(result, ret);
+}
+
+bool Module_UID::UID_Available()
+{
+    return uid != NULL;
+}
+
+void Module_UID::doHealthCheck()
+{
+    QString Id = settings.value("uidId").toString();
+
+    unsigned char sequence[] = {UID::UID_IDENTIFY};
+    char id[9];
+
+    if ( uid == NULL ) {
+        return;
+    }
+
+    if (!SendCommand(sequence, 1, 50)) {
+        setHealthToSick("Writing Opcode failed");
+        return;
+    }
+
+    int bytesRead = uid->read(id, sizeof(id));
+    if ( bytesRead < sizeof(id)) {
+        setHealthToSick("No Id received, read only "+QString::number(bytesRead)+ " bytes.");
+        return;
+    }
+
+    if (QString::fromAscii(id,sizeof(id)-1) != Id) {
+        logger->debug("Received id: " + QString::fromUtf8(id,8)+"; expected: "+Id);
+        setHealthToSick("Wrong Id, received: " + QString::fromUtf8(id,8));
+        return;
+    }
+
+    setHealthToOk();
 }
--- Hanse/Module_UID/module_uid.h
+++ Hanse/Module_UID/module_uid.h
@@ -1,10 +1,15 @@
 #ifndef MODULE_UID_H
 #define MODULE_UID_H
 
-#include <Framework/robotmodule.h>
+#include "robotmodule.h"
+#include "Module_UID_global.h"
 #include "QtUID.h"
 
-class Module_UID : public RobotModule {
+class QextSerialPort;
+class QextPortInfo;
+
+
+class MODULE_UIDSHARED_EXPORT Module_UID : public RobotModule {
     Q_OBJECT
 public:
 
@@ -17,7 +22,272 @@ public:
     // inherited from RobotModule
     QList<RobotModule*> getDependencies();
 
-    UID* getUID();
+    /**
+     *	Enumerator für die I2C Geschwindigkeiten
+     */
+    enum I2CSpeed
+    {
+        kHz100 = 0xCC,
+        kHz200 = 0xF0,
+        khz400 = 0xFF
+    };
+
+    /** \brief  UID Available
+     *
+     *  Überprüft ob UID vorhanden
+     *
+     *  @return                     TRUE, wenn UID gefunden wurde.
+     */
+    bool UID_Available();
+
+    /** \brief  Idendity
+     *
+     *  Fragt die Identifikationskennung des UIDs ab
+     *
+     *  @return                     Name des UIDs.
+     */
+    QString UID_Identify();
+
+    /** \brief  Revision
+     *
+     *  Fragt die Revisionsnummer des UID ab
+     *
+     *  @return                     Revisionsnummer des UID.
+     */
+    QString UID_Revision();
+
+    /** \brief Startet I2C Acknowledge Modus
+     *
+     *  Für Slaves, die nach jedem Byte ein Acknowledge brauchen
+     *  @return                     TRUE wenn Ausführung erfolgreich
+     */
+    bool I2C_EnterAckMode();
+
+    /** \brief Beendet I2C Acknowledge Modus
+     *
+     *  Für Slaves, die nicht nach jedem Byte ein Acknowledge brauchen
+     *  @return                     TRUE wenn Ausführung erfolgreich
+     */
+    bool I2C_LeaveAckMode();
+
+    /** \brief I2C-Scan Methode
+     *
+     *  Scannt nach angeschlossenen I2C-Slaves
+     *
+     *  @return                     Liste der gefundenen Slaves
+     */
+    QVector<unsigned char> I2C_Scan();
+
+    /** \brief I2C-Speed Methode
+     *
+     *  Setzt die Geschwindigkeit für die I2C-Kommunikation (Standard: 200Khz)
+     *
+     *  @param[in]      speed       Geschwindigkeit (100khz, 200khz, 400khz)
+     *  @return                     TRUE wenn Ausführung erfolgreich
+     */
+    bool I2C_Speed(I2CSpeed speed);
+
+    /** \brief I2C-Read Methode
+     *
+     *  Liest Daten von einem Slave
+     *
+     *	@param[in]      address     Die Adresse des Slaves
+     *	@param[in]      byteCount   Anzahl Bytes der zu lesenden Daten
+     *  @param[in]      result      Pointer auf das Zielarray der gelesenen Daten
+     *  @return                     TRUE wenn Lesen erfolgreich
+     */
+    bool I2C_Read(unsigned char address, short byteCount, unsigned char* result);
+
+    /** \brief I2C-Read Register Methode
+     *
+     *  Liest Daten aus einem Register von einem Slave
+     *
+     *	@param[in]      address     Die Adresse des Slaves
+     *  @param[in]      reg         Nummer des zu lesenden Registers
+     *	@param[in]      byteCount   Anzahl Bytes der zu lesenden Daten
+     *  @param[in]  result      Pointer auf das Zielarray der gelesenen Daten
+     *  @return                     TRUE wenn Lesen erfolgreich
+     */
+    bool I2C_ReadRegisters(unsigned char address, unsigned char reg, short byteCount, unsigned char* result);
+
+    /** \brief SRF08 Methode
+     *
+     *  Spezielle Abfragemethode für den SRF08 Ultraschallsensor die es erlaubt zu überprüfen,
+     *  ob eine gestartete Messung beendet wurde.
+     *
+     *	@param[in]      address     Die Adresse des SRF08
+     *  @return                     TRUE wenn SRF08 fertig mit Messung
+     */
+    bool I2C_TestSRF08Ready(unsigned char address);
+
+    /** \brief I2C-Write Methode
+     *
+     *  Sendet Daten an einen Slave
+     *
+     *	@param[in]      address     Die Adresse des Slaves
+     *  @param[in]      data        Array der zu schreibenden Daten
+     *	@param[in]      byteCount   Anzahl Bytes der zu schreibenden Daten
+     *  @return                     TRUE wenn Schreiben erfolgreich
+     */
+    bool I2C_Write(unsigned char address, unsigned char* data, short byteCount);
+
+    /** \brief I2C-Write Register Methode
+     *
+     *  Sendet Daten an Register eines Slaves
+     *
+     *	@param[in]      address     Die Adresse des Slaves
+     *  @param[in]      reg         Zielregister
+     *  @param[in]      data        Array der zu schreibenden Daten
+     *	@param[in]      byteCount   Anzahl Bytes der zu schreibenden Daten
+     *  @return                     TRUE wenn Schreiben erfolgreich
+     */
+    bool I2C_WriteRegister(unsigned char address, unsigned char reg, unsigned char* data, short byteCount);
+
+
+
+
+    /** \brief Direct Access I2C-Start Methode
+     *
+     *  Führt einen Start aus.
+     *  @return                     TRUE wenn Start erfolgreich
+     */
+    bool I2C_DA_Start();
+
+    /** \brief Direct Access I2C-Stop Methode
+     *
+     *  Führt einen Stop aus.
+     *  @return                     TRUE wenn Stop erfolgreich
+     */
+    bool I2C_DA_Stop();
+
+    /** \brief Direct Access I2C-Repeat Start Methode
+     *
+     *  Führt einen Repeat Start aus.
+     *  @return                     TRUE wenn RepStart erfolgreich
+     */
+    bool I2C_DA_RepStart();
+
+    /** \brief Direct Access I2C-Send Ack Methode
+     *
+     *  Empfängt ein Acknowledge vom Slave
+     *  @return                     TRUE wenn Acknowledge erfolgreich empfangen
+     */
+    bool I2C_DA_SendAck();
+    /** \brief Direct Access I2C-Send Ack Methode
+     *
+     *  Sendet ein Acknowledge an den Slave
+     *  @return                     TRUE wenn Acknowledge erfolgreich gesendet
+     */
+    bool I2C_DA_ReceiveAck();
+
+    /** \brief Direct Access I2C-Receive Byte Ack Methode
+     *
+     *  Empfängt ein Byte mit Acknowledge vom Slave
+     *  @return                     TRUE wenn Byte und Acknowledge erfolgreich empfangen
+     */
+    bool I2C_DA_ReceiveByteAck();
+
+    /** \brief Direct Access I2C-Receive Byte Methode
+     *
+     *  Empfängt ein Byte ohne Acknowledge vom Slave
+     *  @return                     TRUE wenn Byte erfolgreich empfangen
+     */
+    bool I2C_DA_ReceiveByteNoAck();
+
+    /** \brief Direct Access I2C-Send Byte Ack Methode
+     *
+     *  Sendet ein Byte mit Acknowledge an Slave
+     *  @return                     TRUE wenn Byte und Acknowledge erfolgreich gesendet
+     */
+    bool I2C_DA_SendByteAck(unsigned char data);
+
+    /** \brief Direct Access I2C-Send Byte Methode
+     *
+     *  Sendet ein Byte ohne Acknowledge an Slave
+     *  @return                     TRUE wenn Byte erfolgreich gesendet
+     */
+    bool I2C_DA_SendByteNoAck(unsigned char data);
+
+
+    /** \brief ADC Methode
+     *
+     *  Liest den AD-Wandler des ATmega aus. Der Kanal, an dessen Stelle in der Bitmaske eine 1 steht wird ausgelesen.
+     *  Mehrere Kanäle können "gleichzeitig" ausgelesen werden.
+     *
+     *  @param[in]      bitmask     Die Bitmaske für den AD-Wandler
+     *  @param[in]      result      Pointer auf das Ergebnisarray
+     *  @return                     TRUE wenn Ausführung erfolgreich
+     */
+    bool UID_ADC(unsigned char bitmask, unsigned char* result);
+
+    /** \brief SPI-Speed Methode
+     *
+     *  Setzt die Geschwindigkeit für die SPI-Kommunikation
+     *
+     *  @param[in]      speed       1=clk/2<br>
+     *                              2=clk/4<br>
+     *                              3=clk/8<br>
+     *                              4=clk/16<br>
+     *                              5=clk/32<br>
+     *                              6=clk/64<br>
+     *                              7=clk/128<br>
+     *  @return                     TRUE wenn Ausführung erfolgreich
+     */
+    bool SPI_Speed(unsigned char speed);
+
+    /** \brief SPI-SetPHA Methode
+     *
+     *  Setzt die Phase für die SPI-Kommunikation
+     *
+     *  @param[in]      phase
+     *  <img src="SPI.png" alt="SPI">
+     *  @return                     TRUE wenn Ausführung erfolgreich
+     */
+    bool SPI_SetPHA(bool phase);
+
+    /** \brief SPI-SetPol Methode
+     *
+     *  Setzt die Polarität für die SPI-Kommunikation
+     *
+     *  @param[in]      pol
+     *  <img src="SPI.png" alt="SPI">
+     *  @return                     TRUE wenn Ausführung erfolgreich
+     */
+    bool SPI_SetPOL(bool pol);
+
+    /** \brief SPI-Read Methode
+     *
+     *  Liest Daten von einem SPI Slave
+     *
+     *	@param[in]      address     Die Adresse des Slaves
+     *	@param[in]      byteCount   Anzahl Bytes der zu lesenden Daten
+     *  @param[in]      result      Pointer auf das Zielarray der gelesenen Daten
+     *  @return                     TRUE wenn Lesen erfolgreich
+     */
+    bool SPI_Read(unsigned char address, short byteCount, unsigned char* result);
+
+    /** \brief SPI-Write Register Methode
+     *
+     *  Schreibt Daten an einen SPI Slave
+     *
+     *	@param[in]      address     Die Adresse des Slaves
+     *  @param[in]      data        Array der zu schreibenden Daten
+     *	@param[in]      byteCount   Anzahl Bytes der zu schreibenden Daten
+     *  @return                     TRUE wenn Schreiben erfolgreich
+     */
+    bool SPI_Write(unsigned char address, unsigned char* data, short byteCount);
+
+    /** \brief SPI-WriteRead Register Methode
+     *
+     *  Schreibt Daten an einen SPI Slave und liest gleichzeitig Ergebnis
+     *
+     *	@param[in]      address     Die Adresse des Slaves
+     *  @param[in]      data        Array der zu schreibenden Daten
+     *	@param[in]      byteCount   Anzahl Bytes der zu schreibenden Daten
+     *  @param[in]      result      Pointer auf das Zielarray der gelesenen Daten
+     *  @return                     TRUE wenn Schreiben erfolgreich
+     */
+    bool SPI_WriteRead(unsigned char address, unsigned char* data, short byteCount, unsigned char* result);
 
 public slots:
     // inherited from RobotModule
@@ -26,7 +296,14 @@ public slots:
     void terminate();
 
 private:
-    UID *uid;
+    QextSerialPort* uid;
+    PortSettings* portSettings;
+
+    QextSerialPort* findUIDPort();
+    QextSerialPort* tryOpenPort(QString id, QextPortInfo* port);
+
+    bool SendCommand(unsigned char* sequence, unsigned char length, int msec);
+    void doHealthCheck();
 
 };
 
