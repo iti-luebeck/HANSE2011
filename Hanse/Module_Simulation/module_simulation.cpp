0b8b49dcdf26ca570e6e77df654ab75a1e35a6e0
tosik <tosik@iti.uni-luebeck.de>
2010-10-14 09:26:24 +0000

    cleaned the simulations module a little bit up
    git-svn-id: svn+ssh://elmi.iti.uni-luebeck.de/export/svn/Hanse/trunk@436 8f13a6d9-6203-4c43-9b9f-a3039379152f

--- Hanse/Module_Simulation/module_simulation.cpp
+++ Hanse/Module_Simulation/module_simulation.cpp
@@ -1,41 +1,18 @@
 #include "module_simulation.h"
 #include "simulation_form.h"
-#include <Module_UID/module_uid.h>
 
-#define REGISTER_CALIB 00 // 8 bytes
-#define REGISTER_PRESSURE_RAW 8
-#define REGISTER_TEMP_RAW 10
-#define REGISTER_PRESSURE 12
-#define REGISTER_TEMP 14
-#define REGISTER_STATUS 17
-#define REGISTER_COUNTER 20
-
-#define REQUEST_NEW_CALIB_VALUES 18
-
-// indicates problem between i2c-spi bridge and pressure sensor
-#define STATUS_MAGIC_VALUE 0x55
-#define CALIB_MAGIC_VALUE 224
-
-// pressure range. everything outside this range will be regarded as
-// a meassurement error
-#define PRESSURE_MIN 900
-#define PRESSURE_MAX 3000
-
-Module_Simulation::Module_Simulation(QString id, Module_UID *uid)
+Module_Simulation::Module_Simulation(QString id)
     : RobotModule(id)
 {
     thread.start();
 
-    this->uid=uid;
+    //this->uid=uid;
 
     setDefaultValue("i2cAddress", 0x50);
     setDefaultValue("frequency", 1);
 
     thread.start();
     //timer.moveToThread(&thread);
-
-    connect(&timer,SIGNAL(timeout()), this, SLOT(refreshData()),
-            Qt::DirectConnection);
 
     reset();
 }
@@ -53,98 +30,11 @@ void Module_Simulation::terminate()
 void Module_Simulation::reset()
 {
     RobotModule::reset();
-
-    int freq = 1000/getSettings().value("frequency").toInt();
-    if (freq>0) {
-        timer.setInterval(freq);
-        QTimer::singleShot(0, &timer, SLOT(start()));
-    } else {
-        QTimer::singleShot(0, &timer, SLOT(stop()));
-    }
-
-    if (!getSettings().value("enabled").toBool())
-        return;
-
-    unsigned char address = getSettings().value("i2cAddress").toInt();
-    char reg = REQUEST_NEW_CALIB_VALUES;
-    if (!uid->I2C_Write(address, &reg, 1)) {
-        setHealthToSick(uid->getLastError());
-    }
-    msleep(100);
-
-}
-
-void Module_Simulation::refreshData()
-{
-    if (!getSettings().value("enabled").toBool())
-        return;
-
-    readPressure();
-    readTemperature();
-
-    if (getHealthStatus().isHealthOk()) {
-        emit dataChanged(this);
-        emit newDepthData(getDepth());
-    }
-}
-
-void Module_Simulation::readPressure()
-{
-    unsigned char readBuffer[2];
-
-    if (!readRegister(REGISTER_PRESSURE, 2, (char*)readBuffer)) {
-        setHealthToSick(uid->getLastError());
-        return;
-    }
-
-    // this is the pressure in mBar
-    uint16_t pressure = (int)readBuffer[0] << 8 | (int)readBuffer[1];
-
-    QMutexLocker l(&this->moduleMutex);
-
-    data["pressure"] =  pressure;
-
-    // 100 mBar == ca. 1m wassersäule - druck an der luft
-    data["depth"] =  ((float)pressure-getSettings().value("airPressure").toFloat())/100;
-
-    if (pressure < PRESSURE_MIN || pressure > PRESSURE_MAX) {
-        setHealthToSick("Pressure of "+QString::number(pressure) + " doesn't make sense.");
-    }
-}
-
-void Module_Simulation::readTemperature()
-{
-
-    unsigned char readBuffer[2];
-    if (!readRegister(REGISTER_TEMP, 2, (char*)readBuffer)) {
-        setHealthToSick(uid->getLastError());
-        return;
-    }
-
-    // this is the temperature in 10/degree celsius
-    uint16_t temp = (int)readBuffer[0] << 8 | (int)readBuffer[1];
-
-    QMutexLocker l(&this->moduleMutex);
-
-    data["temperatureHW"] = ((float)temp)/10;
-}
-
-float Module_Simulation::getDepth()
-{
-    QMutexLocker l(&this->moduleMutex);
-    return data["depth"].toFloat();
-}
-
-float Module_Simulation::getTemperature()
-{
-    QMutexLocker l(&this->moduleMutex);
-    return data["temperature"].toFloat()/10.0;
 }
 
 QList<RobotModule*> Module_Simulation::getDependencies()
 {
     QList<RobotModule*> ret;
-    ret.append(uid);
     return ret;
 }
 
@@ -158,46 +48,5 @@ void Module_Simulation::doHealthCheck()
     if (!isEnabled())
         return;
 
-    char readBuffer[1];
-
-    if (!readRegister(REGISTER_STATUS, 1, readBuffer)) {
-        setHealthToSick(uid->getLastError());
-        return;
-    }
-
-    if (readBuffer[0] != STATUS_MAGIC_VALUE) {
-        setHealthToSick("Status register doesn't match magic value: is="+QString::number(readBuffer[0]));
-        return;
-    }
-
-    // check if the counter is increasing.
-
-    if (!readRegister(REGISTER_COUNTER, 1, readBuffer)) {
-        setHealthToSick(uid->getLastError());
-        return;
-    }
-
-    QMutexLocker l(&this->moduleMutex);
-
-    data["counter"] = (unsigned char)readBuffer[0];
-
-    if (readBuffer[0] == (unsigned char)counter) {
-        setHealthToSick("read the same counter value twice!");
-        return;
-    }
-
-    counter = (unsigned char)readBuffer[0];
-
     setHealthToOk();
-}
-
-bool Module_Simulation::readRegister(unsigned char reg, int size, char *ret_buf)
-{
-    unsigned char address = getSettings().value("i2cAddress").toInt();
-
-    if (!uid->I2C_ReadRegisters(address, reg, size, ret_buf)) {
-        setHealthToSick(uid->getLastError());
-        return false;
-    }
-    return true;
 }
--- Hanse/Module_Simulation/module_simulation.h
+++ Hanse/Module_Simulation/module_simulation.h
@@ -11,58 +11,29 @@ class Module_Simulation : public RobotModule {
     Q_OBJECT
 
 public:
-    Module_Simulation(QString id, Module_UID *uid);
+    Module_Simulation(QString id);
     ~Module_Simulation();
 
     QWidget* createView(QWidget* parent);
 
     QList<RobotModule*> getDependencies();
 
-    /**
-      * Return depth below the surface in meters.
-      *
-      * Range: 0 to infinity
-      *
-      */
-    float getDepth();
-
-    /**
-      * Return temperature of the pressure sensor in degree celsius
-      *
-      */
-    float getTemperature();
-
 public slots:
     void reset();
     void terminate();
 
-
 signals:
     void healthStatusChanged(HealthStatus data);
-
-    /**
-      * This signal is emmited whenever there a new messurement is available.
-      *
-      */
-    void newDepthData(float depth);
 
 protected:
     virtual void doHealthCheck();
 
 private slots:
-    void refreshData();
 
 private:
-    Module_UID *uid;
     QTimer timer;
     QThread thread;
     unsigned int counter;
-
-    void readPressure();
-    void readTemperature();
-
-    bool readRegister(unsigned char reg, int size, char *ret_buf);
-
 };
 
 #endif // MODULE_SIMULATION_H
