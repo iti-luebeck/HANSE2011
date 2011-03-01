b56a8d021b476060b9b7affc5f80a4602d2976be
zenker <zenker@8f13a6d9-6203-4c43-9b9f-a3039379152f>
2011-03-01 01:25:47 +0000

    git-svn-id: svn+ssh://elmi.iti.uni-luebeck.de/export/svn/Hanse/branches/threadv1@583 8f13a6d9-6203-4c43-9b9f-a3039379152f

--- Hanse/Behaviour_GroundFollowing/groundfollowingform.h
+++ Hanse/Behaviour_GroundFollowing/groundfollowingform.h
@@ -1,7 +1,11 @@
 #ifndef GROUNDFOLLOWINGFORM_H
 #define GROUNDFOLLOWINGFORM_H
 
+#include <Behaviour_GroundFollowing/behaviour_groundfollowing.h>
+
 #include <QWidget>
+
+class Behaviour_GroundFollowing;
 
 namespace Ui {
     class GroundFollowingForm;
@@ -12,11 +16,25 @@ class GroundFollowingForm : public QWidget
     Q_OBJECT
 
 public:
-    explicit GroundFollowingForm(QWidget *parent = 0);
+    explicit GroundFollowingForm(QWidget *parent, Behaviour_GroundFollowing *groundfollowing);
     ~GroundFollowingForm();
+
+protected:
+    void changeEvent(QEvent *e);
 
 private:
     Ui::GroundFollowingForm *ui;
+    Behaviour_GroundFollowing *groundfollow;
+
+private slots:
+    void on_saveButton_clicked();
+    void on_startButton_clicked();
+    void on_stopButton_clicked();
+
+signals:
+   void startGroundFollow();
+   void stopGroundFollow();
+
 };
 
 #endif // GROUNDFOLLOWINGFORM_H
