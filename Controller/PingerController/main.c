8322315964f0c995ac1224642d3e0ae6286d50d0
hanse <hanse@8f13a6d9-6203-4c43-9b9f-a3039379152f>
2011-04-06 15:07:52 +0000

    working external oscillator
    git-svn-id: svn+ssh://elmi.iti.uni-luebeck.de/export/svn/Hanse/branches/threadv1@700 8f13a6d9-6203-4c43-9b9f-a3039379152f

--- Controller/PingerController/main.c
+++ Controller/PingerController/main.c
@@ -4,27 +4,40 @@
  *  Created on: Apr 5, 2011
  *      Author: robprak
  */
-#include <inttypes.h>
+// #include <inttypes.h>
 #include <avr/io.h>
-#include <avr/sleep.h>
+// #include <avr/sleep.h>
 #include <util/delay.h>
 
+/*
+ * Enable external 16 MHz oscillator.
+ */
+void set_external_oscillator() {
+	// These bits select the type and start-up time for the crystal or resonator that is
+	// connected to the	XTAL or TOSC pins.
+	OSC.XOSCCTRL = OSC_FRQRANGE_12TO16_gc | OSC_XOSCSEL_XTAL_16KCLK_gc;
+
+	// Enable external clock and disable everything else.
+    OSC.CTRL = OSC_XOSCEN_bm;
+
+	// Page 87: The	external clock source should be allowed time to become stable before
+	// it is selected as source for	the System Clock.
+	while( (OSC.STATUS & OSC_XOSCRDY_bm) == 0) {
+//	  PORTD.OUTTGL = PIN0_bm;
+	}
+
+	// CLK.CTRL is protected, so we have to allow modification by the following command.
+	CCP = CCP_IOREG_gc;
+	// SCLKSEL is used to select the source for the System Clock.
+	CLK.CTRL = CLK_SCLKSEL_XOSC_gc;
+}
 
 int main() {
-	  	  CCP = 0xD8;
-	OSC.XOSCCTRL = OSC_FRQRANGE_12TO16_gc | OSC_XOSCSEL_XTAL_256CLK_gc;
-    OSC.CTRL |= OSC_XOSCEN_bm;
-    OSC.CTRL &= ~OSC_RC2MEN_bm;
-    OSC.CTRL &= ~OSC_RC32MEN_bm;
-
-	  //wait for osc to become ready
-//	  while( (OSC.STATUS & OSC_XOSCRDY_bm) == 0){
-//	    //PORTF.OUTTGL = (1<<6);
-//	    PORTK.OUTTGL = (1<<0); //toggle led   //toggle led
-//	  }
-
 	PORTD.DIRSET |= PIN0_bm;
-	PORTD.OUT &= ~PIN0_bm;
+	PORTD.OUT |= PIN0_bm;
+
+	set_external_oscillator();
+
 	while (1) {
 		_delay_ms(1000);
 		PORTD.OUT |= PIN0_bm;
